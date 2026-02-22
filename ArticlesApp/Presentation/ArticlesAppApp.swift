db2c2f4ad7d30c7869469bc894907e6818b9d5ec
Edwinps <ejps86@gmail.com>
2026-02-22 19:39:09 +0000

    create main project

---
+++ ArticlesApp/Presentation/ArticlesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesAppApp.swift
+//  ArticlesApp
+//
+//  Created by Edwinps on 22/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArticlesAppTests/ArticlesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesAppTests.swift
+//  ArticlesAppTests
+//
+//  Created by Edwinps on 22/2/26.
+//
+
+import Testing
+@testable import ArticlesApp
+
+struct ArticlesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
